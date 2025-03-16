24cdd3d69a5e359dfe1a077a815dfb411b94df35
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-03-16 17:24:46 -0600

    updating some data inputs for the upload

--- src/components/form-elements/input/input-selection.tsx
+++ src/components/form-elements/input/input-selection.tsx
@@ -68,7 +68,7 @@ export const InputSelectionCard: React.FC<InputSelectionCardProps> = ({
             <h5 className='card-title'>
               <label
                 className='d-flex align-items-center'
-                style={{ cursor: !isDisabled ? 'pointer' : 'auto' }}
+                style={{ cursor: !isDisabled ? 'pointer' : 'auto', textTransform: 'none' }}
                 onClick={($e) => $e.stopPropagation()}
               >
                 <input
