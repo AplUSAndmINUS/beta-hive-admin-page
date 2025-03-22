13f33dd44fd7580dbb11b26cc50fbf9f686e4747
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-03-22 17:08:37 -0600

    added ENV vars to test wireup

--- src/services/apis/admin-apis.ts
+++ src/services/apis/admin-apis.ts
@@ -12,19 +12,19 @@ import { calendarSchema } from 'src/services/models/calendar.types';
 // define nonce wpApiSettings globally
 declare const wpApiSettings: { nonce: string };
 
-// Axios instance that is used for local development ONLY (not for production)
-let wpiApiSettingsLocal = '48e6d11d60'; // change when wpApiSettings is different
+// Fallback nonce for local development
+const localNonce = process.env.REACT_APP_LOCAL_NONCE || '48e6d11d60';
+
+// Base URL for the API
+const baseURL = process.env.REACT_APP_STAGING_API_URL || '/wp-json/custom/v1';
 
 // Create an axios instance with the nonce token for WP backend access
 // Comment out either the local or production instance depending on the environment
 export const axiosInstance = axios.create({
-  baseURL: '/wp-json/custom/v1',
+  baseURL: baseURL,
   headers: {
-    // 'X-WP-Nonce': wpiApiSettingsLocal,
     'X-WP-Nonce':
-      typeof wpApiSettings !== 'undefined'
-        ? wpApiSettings.nonce
-        : wpiApiSettingsLocal,
+      typeof wpApiSettings !== 'undefined' ? wpApiSettings.nonce : localNonce,
     'Content-Type': 'application/json',
   },
 });
@@ -33,7 +33,7 @@ export const axiosInstance = axios.create({
 export const getAllGameContent =
   async (): Promise<gameSettingsSchema | null> => {
     try {
-      const response = await axiosInstance.get('/get_all_game_content');
+      const response = await axiosInstance.get('/game_content');
       return response.data;
     } catch (error) {
       console.error('Error fetching game content:', error);
